fadb1642db0530df19b0a1401aa52d3a3e79a201
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 22:41:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function threeSum(arr, target) {
-  //your code here
+  function threeSum(S, target) {
+    S.sort((a, b) => a - b);
+    let closestSum = Infinity;
+    for (let i = 0; i < S.length - 2; i++) {
+        let left = i + 1, right = S.length - 1;
+        while (left < right) {
+            let currentSum = S[i] + S[left] + S[right];
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+            if (currentSum < target) {
+                left++;
+            } else if (currentSum > target) {
+                right--;
+            } else {
+                return currentSum;
+            }
+        }
+    }
+    return closestSum;
 }
+
+//}
 
 module.exports = threeSum;
